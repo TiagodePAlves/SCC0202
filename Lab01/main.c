6be5bbfd7e19d890835a5009cf4546e6eefb98f5
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-10 20:16:17 -0300

    Better scanning format

--- Lab01/main.c
+++ Lab01/main.c
@@ -3,25 +3,12 @@
 #include <string.h>
 
 
-static __inline__
-int remove_newline(char *str) {
-    const size_t len = strlen(str);
-
-    if (len == 0 || str[len-1] != '\n') {
-        return EXIT_FAILURE;
-    }
-    str[len-1] = '\0';
-
-    return EXIT_SUCCESS;
-}
-
 #define BUF 1024ULL
 
 int main(void) {
     char line[BUF] = "";
 
-    for (size_t i = 0; fgets(line, BUF, stdin) != NULL; i++) {
-        remove_newline(line);
+    for (size_t i = 0; scanf("%1024[^\n]\n", line) == 1; i++) {
 
         printf("line[%zu]: %s\n", i, line);
     }
