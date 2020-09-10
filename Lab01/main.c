c218a7d2871523e5c3c372e3c1b2e2851e45a436
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-10 20:38:51 -0300

    Basic parsing

--- Lab01/main.c
+++ Lab01/main.c
@@ -1,17 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <assert.h>
 
+#define inline __inline__
+#define unused(var) (void) var
 
-#define BUF 1024ULL
+#define BUF_SIZE 1024
+
+#define _STR(X) #X
+#define STR(X) _STR(X)
+
+static inline
+int read_line(char buffer[BUF_SIZE]);
 
 int main(void) {
-    char line[BUF] = "";
+    char line[BUF_SIZE] = "";
 
-    for (size_t i = 0; scanf("%1024[^\n]\n", line) == 1; i++) {
+    while (read_line(line)) {
+        char pat[BUF_SIZE] = "";
+        char sub[BUF_SIZE] = "";
 
-        printf("line[%zu]: %s\n", i, line);
+        const bool pat_res = read_line(pat);
+        assert(pat_res); unused(pat_res);
+        const bool sub_res = read_line(sub);
+        assert(sub_res); unused(sub_res);
+
+        int res = printf("%s <-| %s -> %s\n", line, pat, sub);
+        assert(res > 0); unused(res);
     }
 
     return EXIT_SUCCESS;
+}
+
+static inline
+int read_line(char buffer[BUF_SIZE]) {
+    const int result = scanf("%"STR(BUF_SIZE)"[^\n]\n", buffer);
+    return result == 1;
 }
