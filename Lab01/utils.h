56b1719255c0b46430b06a4e529b4edeca808805
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-11 14:54:17 -0300

    FIX: new 'assertion_eq' that marks unused RHS

--- Lab01/utils.h
+++ Lab01/utils.h
@@ -2,14 +2,24 @@
 /* Shared utility macros. */
 #define __UTILS_H__
 
+#include <assert.h>
+
+
 // See https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
 #define __STRINGIFY(token) #token
 /* Turns a token or macro into literal string. */
 #define STR(token) __STRINGIFY(token)
 
-// Change behaviour of assert on non-debug builds to avoid unused errors
-#ifdef NDEBUG
-#define assert(expr) ((void) expr)
+#ifndef NDEBUG
+/* Assertion for variable values on debug builds. On non-debug builds
+ * this casts RHS as `void`, to mark variables as unused.
+ */
+#define assert_eq(variable, expected) \
+    assert((variable) == (expected))
+#else
+// Change behaviour on non-debug builds to avoid unused errors
+#define assert_eq(variable, expected) \
+    (void) variable
 #endif
 
 /* Compiler attributes declaration. */
