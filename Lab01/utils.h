0118176a01c9c08689ccb1833f62239f17ddae40
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-11 15:37:36 -0300

    Better line spacing

--- Lab01/utils.h
+++ Lab01/utils.h
@@ -5,10 +5,11 @@
 #include <assert.h>
 
 
-// See https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
+// https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
 #define __STRINGIFY(token) #token
 /* Turns a token or macro into literal string. */
 #define STR(token) __STRINGIFY(token)
+
 
 #ifndef NDEBUG
 /* Assertion for variable values on debug builds. On non-debug builds
@@ -21,6 +22,7 @@
 #define assert_eq(variable, expected) \
     (void) variable
 #endif
+
 
 /* Compiler attributes declaration. */
 #define attribute(...) __attribute__((__VA_ARGS__))
