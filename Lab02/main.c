fd2e9267169423c0e52ebe189f122c839b60e6ba
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-11 19:49:02 -0300

    Initial implementation

---
+++ Lab02/main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "utils.h"
+
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
---
+++ Lab02/utils.h
@@ -0,0 +1,40 @@
+#ifndef __UTILS_H__
+/* Shared utility macros. */
+#define __UTILS_H__
+
+#include <assert.h>
+
+
+// https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
+#define __STRINGIFY(token) #token
+/* Turns a token or macro into literal string. */
+#define STR(token) __STRINGIFY(token)
+
+
+#ifndef NDEBUG
+/* Assertion for variable values on debug builds. On non-debug builds
+ * this casts RHS as `void`, to mark variables as unused.
+ */
+#define assert_eq(variable, expected) \
+    assert((variable) == (expected))
+#else
+// Change behaviour on non-debug builds to avoid unused errors
+#define assert_eq(variable, expected) \
+    (void) variable
+#endif
+
+
+/* Compiler attributes declaration. */
+#define attribute(...) __attribute__((__VA_ARGS__))
+
+
+/* Gets the maximum of two integers. */
+#define max(x, y) \
+    (((x) >= (y))? (x) : (y))
+
+/* Gets the minimum of two integers. */
+#define min(x, y) \
+    (((x) <= (y))? (x) : (y))
+
+
+#endif/*__UTILS_H__*/
