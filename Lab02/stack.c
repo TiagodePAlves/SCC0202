a17daf231b8c606db718f2892328667766de89d2
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-13 17:07:03 -0300

    Stack implementation

---
+++ Lab02/stack.c
@@ -0,0 +1,206 @@
+#include "stack.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <string.h>
+
+
+// Initial capacity for a new stack.
+#define INITIAL_CAPACITY 64
+
+
+struct stack {
+    // capacity power of two
+    size_t cap, len;
+    size_t elem_size;
+    byte_t data[];
+};
+
+/* * * * * * * * *
+ * Basic checks  */
+
+static inline attribute(pure, nonnull)
+// True when stack is empty.
+bool empty(const_stack_t stack) {
+    assert(stack != NULL);
+
+    return stack->len == 0;
+}
+
+static inline attribute(pure, nonnull)
+// True when stack capacity is full.
+bool full(const_stack_t stack) {
+    assert(stack != NULL);
+    // check length validity on debug
+    assert(stack->len <= stack->cap);
+
+    return stack->len >= stack->cap;
+}
+
+static inline attribute(pure, nonnull)
+// Indicates low capacity usage (less than 25%) by the stack.
+// Used for shrinking.
+bool low_usage(const_stack_t stack) {
+    assert(stack != NULL);
+    // don't shrink when at initial capacity
+    bool small_cap = stack->cap <= INITIAL_CAPACITY;
+    // otherwise, shrink when using less than 25% capacity
+    bool low_len = stack->len < stack->cap / 4;
+    return !small_cap && low_len;
+}
+
+attribute(pure, nonnull)
+// extern version
+bool stack_empty(const_stack_t stack) {
+    return empty(stack);
+}
+
+/* * * * * * * * * * *
+ * Memory management */
+
+// ceil(n / m) for integers, instead of the default integer division
+// https://en.wikipedia.org/wiki/Floor_and_ceiling_functions#Quotients
+#define ceil_div(n, m) \
+    (((n) - 1) / (m) + 1)
+
+static inline attribute(const)
+// Calculates necessary allocation size for a stack that holds
+// `capacity` elements of size `elem_size`.
+size_t size_for(size_t elem_size, size_t capacity) {
+    // size of the fixed part of the struct
+    const size_t fixed = offsetof(struct stack, data);
+    // overflow check on debug
+    assert(ceil_div(SIZE_MAX - fixed, elem_size) >= capacity);
+
+    return fixed + elem_size * capacity;
+}
+
+static inline attribute(const)
+// Next capacity when growing a stack.
+size_t next_capacity(size_t current) {
+    if (current == 0) {
+        return INITIAL_CAPACITY;
+    } else {
+        // overflow check on debug
+        assert(current <= SIZE_MAX / 2);
+        return current * 2;
+    }
+}
+
+// Creates the stack
+stack_t stack_new(size_t elem_size) {
+    stack_t new = malloc(size_for(elem_size, INITIAL_CAPACITY));
+    if (unlikely(new == NULL)) {
+        return NULL;
+    }
+    new->cap = INITIAL_CAPACITY;
+    new->len = 0;
+    new->elem_size = elem_size;
+    return new;
+}
+
+static inline attribute(nonnull)
+// Increase stack capacity, moving to another pointer
+stack_t grow(stack_t stack) {
+    assert(stack != NULL);
+
+    size_t new_cap = next_capacity(stack->cap);
+    size_t new_size = size_for(stack->elem_size, new_cap);
+
+    stack_t new = realloc(stack, new_size);
+    if (unlikely(new == NULL)) {
+        return NULL;
+    }
+    new->cap = new_cap;
+    return new;
+}
+
+static inline attribute(nonnull)
+// Reduces stack capacity, moving to another pointer
+//
+// NOTE: Doesn't update stack length, so this must be checked before shrinking
+stack_t shrink(stack_t stack) {
+    assert(stack != NULL);
+
+    size_t new_cap = stack->cap / 2;
+    size_t new_size = size_for(stack->elem_size, new_cap);
+
+    stack_t new = realloc(stack, new_size);
+    if (unlikely(new == NULL)) {
+        return NULL;
+    }
+    new->cap = new_cap;
+    // check length is valid on debug
+    assert(new->len <= new_cap);
+    return new;
+}
+
+static inline attribute(pure, nonnull)
+// Get pointer to element at position `pos` on the stack.
+//
+// NOTE: does not check bounds
+void *get_elem(const_stack_t stack, size_t pos) {
+    assert(stack != NULL);
+
+    size_t byte_pos = stack->elem_size * pos;
+    return (void *) &stack->data[byte_pos];
+}
+
+static inline attribute(nonnull)
+// Read element at position `pos` on the stack, writing to `elem`.
+void read(const_stack_t restrict stack, size_t pos, void * restrict elem) {
+    memcpy(elem, get_elem(stack, pos), stack->elem_size);
+}
+
+static inline attribute(nonnull)
+// Write `elem` at position `pos` on the stack.
+void write(stack_t restrict stack, size_t pos, const void * restrict elem) {
+    memcpy(get_elem(stack, pos), elem, stack->elem_size);
+}
+
+/* * * * * * * * * * *
+ * Stack operations  */
+
+attribute(nonnull)
+bool stack_push(stack_t * restrict stack, const void * restrict elem) {
+    assert(stack != NULL);
+
+    if (unlikely(full(*stack))) {
+        stack_t new = grow(*stack);
+        if (unlikely(new == NULL)) {
+            return false;
+        }
+        *stack = new;
+    }
+
+    write(*stack, (*stack)->len++, elem);
+    return true;
+}
+
+attribute(pure, nonnull)
+void *stack_peek(const stack_t * restrict stack) {
+    assert(stack != NULL);
+
+    if (unlikely(empty(*stack))) {
+        return NULL;
+    }
+    return get_elem(*stack, (*stack)->len-1);
+}
+
+attribute(nonnull)
+bool stack_pop(stack_t * restrict stack, void * restrict elem) {
+    assert(stack != NULL);
+
+    if (unlikely(empty(*stack))) {
+        return false;
+    }
+    read(*stack, --(*stack)->len, elem);
+
+    if (unlikely(low_usage(*stack))) {
+        stack_t new = shrink(*stack);
+        if (likely(new != NULL)) {
+            *stack = new;
+        }
+    }
+    return true;
+}
---
+++ Lab02/stack.h
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "utils.h"
+
+/* Stack as a pointer type. */
+typedef struct stack *stack_t;
+/* Stack as a immutable pointer. */
+typedef const struct stack *const_stack_t;
+
+/* Create stack for a specific type of element. */
+#define stack_for(type) \
+    stack_new(sizeof(type))
+
+/* Creates stack for elements of `elem_size`.
+ *
+ * # Example
+ *
+ * For a stack of bytes, on a x86 archtecture, this would
+ * be created with `stack_new(1)`.
+ */
+stack_t stack_new(size_t elem_size);
+
+/* Checks that a stack is empty. */
+bool stack_empty(const_stack_t stack)
+attribute(pure, nonnull);
+
+/* Push element onto the stack.
+ * Return wether operation was successful.
+ *
+ * `elem` must point to an object of same size as the declared
+ * `elem_size` of the given `stack`.
+ *
+ * If `stack` is full, this function will try to grow the stack
+ * capacity before pushing the element. Operation will only fail
+ * if stack is full and can't be grown.
+ */
+bool stack_push(stack_t * restrict stack, const void * restrict elem)
+attribute(nonnull);
+
+/* Get reference to the top element on the stack.
+ * Returns `NULL` when stack is empty.
+ */
+void *stack_peek(const stack_t * restrict stack)
+attribute(pure, nonnull);
+
+/* Pop top element from the stack.
+ * Return wether operation was successful.
+ *
+ * If stack is empty, nothing is written on `elem` and operation
+ * is considered unsuccessful.
+ *
+ * `elem` must point be able to hold an object of same size as the
+ * declared `elem_size` of the given `stack`.
+ *
+ * If `stack` is using less than 25% of the capacity, the stack
+ * allocated memory may be shrunk.
+ */
+bool stack_pop(stack_t * restrict stack, void * restrict elem)
+attribute(nonnull);
