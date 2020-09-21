ccf4c37ef8d20d00128e971c5347752e7ad5ce99
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-21 20:51:34 -0300

    Dados booleanos

---
+++ Lab03/bool.h
@@ -0,0 +1,14 @@
+#ifndef __BOOL_H__
+/* Dados booleanos. */
+#define __BOOL_H__
+// equivalente ao <stdbool.h>
+
+// Dado booleano
+#define bool  _Bool
+
+// Veradeiro
+#define true  1
+// Falso
+#define false 0
+
+#endif//__BOOL_H__
