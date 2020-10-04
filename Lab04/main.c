58618c60ef8c651a923841d84ba02450dbc16f95
Tiago de Paula <tiagodepalves@gmail.com>
2020-10-04 18:19:24 -0300

    Lab04 - Inicial

---
+++ Lab04/main.c
@@ -0,0 +1,14 @@
+/*
+ * SCC0202 - 2020 2S
+ * // TODO
+ *
+ * Nome: William Maehara
+ * Código: 6792263
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
