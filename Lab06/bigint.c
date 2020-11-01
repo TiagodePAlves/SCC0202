978ecf1b7b5a084c5c60a0fd39f3d5cdf2797fe6
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-01 20:58:41 -0300

    Descrição dos bigint

---
+++ Lab06/bigint.c
@@ -0,0 +1,12 @@
+#include "bigint.h"
+
+// TODO: limits.h
+typedef struct digit {
+    size_t digit;
+    digit_t *prox;
+} digit_t;
+
+
+typedef struct bigint {
+
+} bigint_t;
---
+++ Lab06/bigint.h
@@ -0,0 +1,14 @@
+#ifndef __BIGINT_H__
+#define __BIGINT_H__
+
+#include "utils.h"
+#include <stdio.h>
+
+typedef struct bigint bigint_t;
+
+
+// bigint_t *bigint_read(FILE *stream);
+
+// int bigint_write(FILE *restrict stream, const bigint_t *restrict number);
+
+#endif //__BIGINT_H__
