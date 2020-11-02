9fee4f0ec961360f8609801ecbc9aba6d662f23e
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-01 21:34:41 -0300

    Estruturas do dicionário

--- Proj1/dicio.c
+++ Proj1/dicio.c
@@ -1,9 +1,18 @@
 #include "dicio.h"
 #include <stdlib.h>
 
+#define NIVEL_MAX 32
+
+
+typedef struct palavra {
+    char *chave;
+    char *descricao;
+    size_t nivel;
+    palavra_t *prox[];
+} palavra_t;
 
 typedef struct dicio {
-    int x;
+    palavra_t *ini[NIVEL_MAX];
 } dicio_t;
 
 
