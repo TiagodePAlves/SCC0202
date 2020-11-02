691c234f1e2605e9fce0a3d4fe82b53ba9b256da
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-02 02:03:59 -0300

    FIX: documentação desatualizada

--- Proj1/random.h
+++ Proj1/random.h
@@ -15,10 +15,10 @@ uint8_t __max_hl(void) attribute(const);
 /**
  * Gerador de números com probabilidade de meia vida.
  *
- * Isto é, 0 tem 50% de chance, 1 tem 25%, e assim
- * por diante. Em geral, um número 'n' tem '1/2^(n+1)'
+ * Isto é, `0` tem 50% de chance, `1` tem 25%, e assim
+ * por diante. Em geral, um número 'n' tem "1/2^(n+1)"
  * de probabilidade. O maior número possível é
- * 'MAX_HL', que em geral é 32.
+ * 'MAX_HL', que em geral é 31.
  */
 uint8_t rand_hl(void);
 
