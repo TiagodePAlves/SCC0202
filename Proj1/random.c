b78657b3dedf769d353738ebdf7170b1774afa49
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-01 22:04:25 -0300

    Contador de ticks (x86)

--- Proj1/random.c
+++ Proj1/random.c
@@ -1,3 +1,13 @@
 #include "random.h"
-#include <stdlib.h>
+#include "utils.h"
 
+
+uint64_t ticks(void) {
+    asm("rdtsc");
+    register uint32_t lo asm("eax");
+    register uint32_t hi asm("edx");
+
+    uint64_t low = (uint64_t) lo;
+    uint64_t high = (uint64_t) hi;
+    return (high << 32) | low;
+}
