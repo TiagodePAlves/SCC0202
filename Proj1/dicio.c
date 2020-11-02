93d9b4f33209581b7dd780aa5df8dbed2a6e3f50
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-01 21:07:51 -0300

    Definições iniciais do dicionário

---
+++ Proj1/dicio.c
@@ -0,0 +1,12 @@
+#include "dicio.h"
+#include <stdlib.h>
+
+
+typedef struct dicio {
+    int x;
+} dicio_t;
+
+
+dicio_t *dicio_novo(void) {
+    return NULL;
+}
---
+++ Proj1/dicio.h
@@ -0,0 +1,13 @@
+#ifndef __DICIO_H__
+#define __DICIO_H__
+
+#include "utils.h"
+
+
+typedef struct dicio dicio_t;
+
+
+dicio_t *dicio_novo(void);
+
+
+#endif //__DICIO_H__
