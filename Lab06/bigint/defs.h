18a2f6422a0e71292e9ef7f885a44add05397bfb
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-04 22:05:22 -0300

    Inicializações

--- Lab06/bigint/defs.h
+++ Lab06/bigint/defs.h
@@ -7,17 +7,22 @@
 #include <stdio.h>
 #include <string.h>
 
+
 typedef struct digit digit_t;
 
 struct digit {
-    uint64_t digit;
+    uint64_t digito;
     digit_t *prox;
 };
 
 struct bigint {
-    digit_t number;
+    digit_t numero;
     bool neg;
 };
 
+digit_t *num_alloc_digito(uint64_t digit);
+
+digit_t *num_digito_copia(const digit_t *digito, bool incr)
+attribute(nonull);
 
 #endif //__BIGINT_DEFS__
