cb6491cd100891a0fcf2f81421b04e09dac7a883
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-06 03:43:08 -0300

    <limits.h>

--- Proj1/utils.h
+++ Proj1/utils.h
@@ -5,12 +5,17 @@
 // Lista de atributos da função.
 #define attribute(...) __attribute__((__VA_ARGS__))
 
-// Inteiro de tamanho fixo, sem sinal.
 #ifdef __GNUC__
+// Inteiro de tamanho fixo, sem sinal.
 typedef __UINT8_TYPE__ uint8_t;
 typedef __UINT16_TYPE__ uint16_t;
 typedef __UINT32_TYPE__ uint32_t;
 typedef __UINT64_TYPE__ uint64_t;
+// Limite dos inteiros.
+#define UINT8_MAX __UINT8_MAX__
+#define UINT16_MAX __UINT16_MAX__
+#define UINT32_MAX __UINT32_MAX__
+#define UINT64_MAX __UINT64_MAX__
 #else
 #error "compilador inválido"
 #endif
