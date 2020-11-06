e6bde0cec9fe9b5703c5f9eb66bfaa144c682129
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-06 01:22:18 -0300

    Documentações gerais do lab06

--- Lab06/bigint.h
+++ Lab06/bigint.h
@@ -1,23 +1,59 @@
 #ifndef __BIGINT_H__
+/* Inteiro de largura dinâmica. */
 #define __BIGINT_H__
 
 #include <stdio.h>
 
+// Tipo do inteiro de largura dinâmica.
 typedef struct bigint bigint_t;
 
-
+/**
+ * Cria um 'bigint' com valor zero.
+ *
+ * Encerra o programa em erro de alocação.
+ */
 bigint_t *bigint_alloc(void);
-
+/**
+ * Cria uma cópia de um 'bigint'.
+ *
+ * Encerra o programa em erro de alocação.
+ */
 bigint_t *bigint_copia(const bigint_t *num);
-
+/**
+ * Desaloca um 'bigint'.
+ */
 void bigint_dealloc(bigint_t *num);
-
+/**
+ * Comparação de dois inteiros.
+ *
+ * Retorna:
+ *  - negativo:  se 'lhs < rhs'
+ *  - zero    :  se 'lhs == rhs'
+ *  - positivo:  se 'lhs > rhs'
+ */
 int bigint_cmp(const bigint_t *lhs, const bigint_t *rhs);
-
+/**
+ * Soma dois inteiros. O resultado é alocado em
+ * um novo 'bigint.
+ *
+ * Encerra o programa em erro de alocação.
+ */
 bigint_t *bigint_add(const bigint_t *lhs, const bigint_t *rhs);
-
+/**
+ * Leitura de um 'bigint' decimal em um file stream.
+ *
+ * Espaços antes do número são ignorados, assim como zeros
+ * à esquerda. Faz a leitura de um caracter a mais, após o
+ * número, que é ignorado.
+ *
+ * Encerra o programa em erro de alocação.
+ */
 bigint_t *bigint_read(FILE *stream);
-
+/**
+ * Escrita de um 'bigint' decimal em um file stream.
+ *
+ * Encerra o programa em erro de alocação.
+ */
 void bigint_show(FILE *stream, const bigint_t *num);
 
 #endif //__BIGINT_H__
--- Lab06/bigint/defs.h
+++ Lab06/bigint/defs.h
@@ -1,4 +1,5 @@
 #ifndef __BIGINT_DEFS__
+/* Tipos e funções internas do 'bigint'. */
 #define __BIGINT_DEFS__
 
 #include "../bigint.h"
@@ -8,26 +9,37 @@
 #include <string.h>
 
 
+// Lista ligada dos dígitos.
 typedef struct num num_t;
+// Inteiro que armazena um dígito decimal.
 typedef uint8_t digito_t;
 
+// Base decimal utilizada para o 'bigint'.
 #define BASE    ((digito_t) 10)
-#define MAX     ((digito_t) 9)
 
-
+// O inteiro de largura dinâmica é basicamente
+// uma lista ligada de dígitos BCD.
+// O primeiro elemento é o menos significativo.
 struct num {
     digito_t digito;
     num_t *prox;
 };
 
+// O 'bigint_t' contém a cabeça da lista,
+// já que deve sempre existir ao menos um
+// dígito, e o sinal do número.
 struct bigint {
+    // lista com os dígitos
     num_t numero;
+    // se o número é negativo
     bool neg;
 };
 
-
-bigint_t *bigint_alloc(void);
+// Alocação de um nó da lista de números,
+// com o dado dígito.
 num_t *num_alloc(digito_t digito);
+// Desaloca um nó da lista, e todos os nós
+// subsequentes.
 void num_dealloc(num_t *num);
 
 #endif //__BIGINT_DEFS__
