dab95cc241dd39b10049e231baed815cda75405b
Tiago de Paula <tiagodepalves@gmail.com>
2020-11-06 20:32:23 -0300

    DOC: fsb

--- Proj1/random.c
+++ Proj1/random.c
@@ -91,7 +91,7 @@ uint32_t rand_int(void) {
 
 static inline attribute(const)
 /**
- * First set bit de um `num` positivo.
+ * First set bit de um número positivo.
  */
 uint8_t fsb(uint32_t num) {
     uint8_t pos = (uint8_t) __builtin_ffs((int) num);
