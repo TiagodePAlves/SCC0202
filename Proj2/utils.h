84c3c0ecb3950937d2573c5e9fc68e46b01bfa34
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-05 09:57:54 -0300

    Desnecessário

--- Proj2/utils.h
+++ Proj2/utils.h
@@ -5,7 +5,6 @@
 // Lista de atributos da função.
 #define attribute(...) __attribute__((__VA_ARGS__))
 
-#ifdef __GNUC__
 // Inteiro de tamanho fixo, sem sinal.
 typedef __UINT8_TYPE__ uint8_t;
 typedef __UINT16_TYPE__ uint16_t;
@@ -16,9 +15,6 @@ typedef __UINT64_TYPE__ uint64_t;
 #define UINT16_MAX __UINT16_MAX__
 #define UINT32_MAX __UINT32_MAX__
 #define UINT64_MAX __UINT64_MAX__
-#else
-#error "compilador inválido"
-#endif
 
 // Marcador de branch provável.
 #define likely(x) \
