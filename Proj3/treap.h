f5ca35621d2faecaa966c6622194d3f693f9eb86
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-07 13:46:27 -0300

    Treap declaração

---
+++ Proj3/treap.h
@@ -0,0 +1,22 @@
+#ifndef __TREAP_H__
+#define __TREAP_H__
+
+#include "utils.h"
+
+typedef uint64_t chave_t;
+
+struct treap {
+    struct node *raiz;
+};
+
+struct treap treap_nova(void);
+
+void treap_dealloc(struct treap *arvore);
+
+int treap_insere(struct treap *arvore, chave_t chave);
+
+bool treap_busca(const struct treap *arvore, chave_t chave);
+
+int treap_remove(struct treap *arvore, chave_t chave);
+
+#endif //__TREAP_H__
